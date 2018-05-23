d2addcba4c13571030842f89f7524179aa503bd7
stone005 <39050790+stone005@users.noreply.github.com>
2018-05-23 15:34:42 +0200

    Update urls.py
    Aggiunto nuova url

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -6,5 +6,6 @@ from django.contrib.auth.views import login, logout
 urlpatterns = [
     url(r'^$', views.home, name="home"),
     url(r'^login/$', login, {'template_name': 'accounts/login.html'}),
-    url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'})
+    url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}),
+    url(r'^register/$', views.register, name='register')
 ]
